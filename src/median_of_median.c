5c3746d31d4e479050c999be6c7763e176ebac1d
Nakamura Syo <nakamura.s.bi@is-mac16.edu.c.titech.ac.jp>
2019-07-02 11:36:37 +0900

    途中

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -32,6 +32,45 @@ int quick_select(int A[], int n, int k){
   else return quick_select(A+1, j-1, k);
 }
 
+int divided(int A[], int n, int k){
+    int i, j, pivot, C[], D[];
+    
+    // 真ん中の要素をピボットとする
+    pivot = A[n/2];
+    A[n/2] = A[0];
+    A[0] = pivot;
+    for(i = j = 1; i < n; i++){
+        if(A[i] <= pivot){
+            swap(A+i, A+j);
+            j++;
+        }
+    }
+    for(i = 1; i < j; i++){
+        C[i] = A[i]
+    }
+    for(i = j; i < n; i++){
+        D[i] = A[i]
+    }
+}
+    
+int median_of_median(int A[], int n){
+    int i, j, l, B[N], pivot;
+    if(n <= 5) return quick_select(A+0, n, n/2);
+    else for(i = 0; i < n; i++){
+        B[i] = A[i];
+    }
+    l = n;
+        for(i = j = 0; 5*i < l - 5; i++){
+            B[i] = quick_select(B + 5*j, 5, 2)
+            j++;
+        }
+        B[j] = quick_select(B+5*j, l-5*j, (l-5*j)/2);
+    l = j+1;
+    pivot = quick_select(B+0, l, l/2)
+    if(
+    
+}
+
 
 int main(){
   int i;
@@ -41,7 +80,7 @@ int main(){
     A[i] = (long long int) A[i-1] * A[1] % N;
   }
   for(i=0;i<N;i++){
-    if(quick_select(A, N, i) != i) printf("ERROR %d %d\n", i, quick_select(A, N, i));
+    if(median_of_median(A, N) != quick_select(A, N, N/2)) printf("ERROR");
 //    printf("%d th element is %d\n", i, quick_select(A, N, i));
   }
 }
